6d7402d51f50d84739864c77794bf26bfbc6f95e
olee2 <oleaeik@protonmail.com>
2023-05-05 19:37:19 +0200

    React style error handling on img

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useRef } from "react";
 import styles from "./Card.module.css";
 import placeholder from "../../assets/placeholder.jpeg";
 
 export const Card = (props) => {
   const { media, name, description, price } = props;
+  const imageRef = useRef();
 
   const backgroundImage = media[0] || placeholder;
   return (
@@ -12,8 +13,9 @@ export const Card = (props) => {
       <img
         src={backgroundImage}
         alt=""
+        ref={imageRef}
         className={styles.cardImage}
-        onerror="this.src='placeholder.jpeg'"
+        onError={() => (imageRef.current.src = "placeholder.jpeg")}
       />
       <div className={styles.textContainer}>
         <h2>{name}</h2>
