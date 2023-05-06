92c6ac15a9d80446e3e389d9301b1f816a98b038
olee2 <oleaeik@protonmail.com>
2023-05-06 20:06:08 +0200

    Carousel

---
+++ src/components/ImageCarousel/index.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import styles from "./ImageCarousel.module.css";
+
+const ImageCarousel = ({ images }) => {
+  return (
+    <Carousel
+      className={styles.carousel}
+      showArrows
+      showStatus={false}
+      showThumbs={false}
+    >
+      {images.map((img, index) => (
+        <div key={index} className={styles.carouselItem}>
+          <img src={img} alt={`Carousel image ${index}`} />
+        </div>
+      ))}
+    </Carousel>
+  );
+};
+
+export default ImageCarousel;
